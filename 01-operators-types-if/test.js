bad7517c304a46f1e2e0a6f80add13239add6156
KushnirDanil <dankusnir09@gmail.com>
2025-03-01 14:14:01 +0200

    Lesson_1

---
+++ 01-operators-types-if/test.js
@@ -0,0 +1,21 @@
+// Коментар на 1 рядок 
+
+/*
+Коментар 
+На декілька
+Рядків
+*/
+
+// Зсіні
+let name = 'Vany';
+let namber = 423;
+let nambeR = 345;
+let fullName = 'Петро Петренко';
+let full_name = 'Петро Петренко'; // Неправильно
+let myList = [1,2,536,734];
+let ,meDic = {'name':'Давид', age:25};
+let b = true;
+
+number = namber + nambeR;
+
+const Name = "Daniil"
