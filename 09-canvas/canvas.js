19f83efc6da613a566c04fc45b69ffbf876e7f4b
KushnirDanil <dankusnir09@gmail.com>
2025-05-10 12:51:06 +0300

    add

--- 09-canvas/canvas.js
+++ 09-canvas/canvas.js
@@ -1,1 +1,6 @@
-const canvas1 = document.getElementById('canvas1')
+const canvas1 = document.getElementById('canvas1');
+const ctx1 = canvas1.getContext('2d');
+
+ctx1.fillRect(20, 20, 100, 100)
+ctx1.clearRect(40, 40, 60, 60)
+ctx1.strokeRect(45, 45, 50, 50)
