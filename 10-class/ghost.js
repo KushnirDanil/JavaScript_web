395a2e7da70c1de08761193d27e73a80e7d6337a
KushnirDanil <dankusnir09@gmail.com>
2025-05-17 14:18:07 +0300

    add

--- 10-class/ghost.js
+++ 10-class/ghost.js
@@ -2,5 +2,5 @@ const canvas = document.getElementById('ghost');
 const ctx = canvas.getContext('2d');
 const cWidth = canvas.width;
 const cHeight = canvas.height;
-console.log(canvas)
-
+//console.log(canvas)
+const imgFolder = "images/"
