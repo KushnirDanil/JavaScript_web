444e3ed6a65dea38c2baa4c0696dfeb120bbd2b9
KushnirDanil <dankusnir09@gmail.com>
2025-06-21 13:51:28 +0300

    add

--- 13-15-photo-keeper/Static/JS/pk.js
+++ 13-15-photo-keeper/Static/JS/pk.js
@@ -1,1 +1,15 @@
-let s = 1;
+console.log('Start!')
+
+class MainMenu{
+    constructor(triggerSelector, containerSelector){
+        this.trigger = document.querySelector(triggerSelector)
+        this.container = document.querySelector(containerSelector)
+        this.init()
+    }
+    init(){
+
+    }
+}
+
+let m = new MainMenu()
+
