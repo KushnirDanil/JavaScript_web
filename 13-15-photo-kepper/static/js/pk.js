a993b50aff548b8ebbe6040c75cce4d229a54740
KushnirDanil <dankusnir09@gmail.com>
2025-09-06 12:22:28 +0300

    add

--- 13-15-photo-kepper/static/js/pk.js
+++ 13-15-photo-kepper/static/js/pk.js
@@ -1,3 +1,5 @@
+console.log('Start!')
+
 class MainMenu {
     constructor(triggerSelector, containeerSelector){
         this.trigger = document.querySelector(triggerSelector)
@@ -11,55 +13,56 @@ class MainMenu {
     }
 }
 
+
 class PhotoGallery{
-    constructor (){
+    constructor(){
         this.allComments = [
-    'Цей кадр нереально крутий! :)',
-    'Ти вмієш дивувати! Кожен кадр - поєднання життєлюбності і краси',
-    'Спинися мить, прекрасна ти!',
-    'Просто супер! Як тобі це вдається?',
-    'Це просто шедевр мистецтва',
-    'В цьому штучному світі так приємно знайти щось натуральне))',
-    'Клас!!!))',
-    'Нереально чудово!',
-    'А ти вмієш дивувати ;)',
-    'Це фото так і проситься в рамочку на стіну'
-    ];
+            'Цей кадр нереально крутий! :)',
+            'Ти вмієш дивувати! Кожен кадр - поєднання життєлюбності і краси',
+            'Спинися мить, прекрасна ти!',
+            'Просто супер! Як тобі це вдається?',
+            'Це просто шедевр мистецтва',
+            'В цьому штучному світі так приємно знайти щось натуральне))',
+            'Клас!!!))',
+            'Нереально чудово!',
+            'А ти вмієш дивувати ;)',
+            'Це фото так і проситься в рамочку на стіну'
+        ];
+        
         this.allDescriptions = [
-    'Коли радості немає меж',
-    'Любов в кожному пікселі',
-    'Фото заряджене позитивом',
-    'Зловив дзен',
-    'Як мало потрібно для щастя',
-    'Знали б ви що в мене на умі! ;)',
-    'Show must go on',
-    'Good vibes only',
-    'My inspiration',
-    'On my way to paradise',
-    'Що це, якщо не любов? Х)'
-    ];
+            'Коли радості немає меж',
+            'Любов в кожному пікселі',
+            'Фото заряджене позитивом',
+            'Зловив дзен',
+            'Як мало потрібно для щастя',
+            'Знали б ви що в мене на умі! ;)',
+            'Show must go on',
+            'Good vibes only',
+            'My inspiration',
+            'On my way to paradise',
+            'Що це, якщо не любов? Х)'
+        ];
         this.picturesDB = this.generatePicturesDB(25)
         console.log(this.picturesDB)
         this.pictureContainer = document.querySelector('.picturesContainer')
     }
-
     randomElement(array){
         return array[Math.floor(Math.random() * array.length)]
     }
+
     generatePicturesDB(number){
         const pictures = []
-
-        for(let i = 0; i < number; i++){
-            const comments =[]
+        
+        for (let i = 0; i < number; i++){
+            const comments = []
             const commentCount = Math.floor(Math.random() * 5)
-            
-            for (let j = 0; j < commentCount; j++){
-                comments.push(this.randomElement(this.allComments))
-            }
 
-            pictures.push ({
-                src: `../static/img/photos/${i}0.jpg`,
-                likes: Math.floor(Math.random()*100),
+            for (let j = 0; j < commentCount; j++)
+                comments.push(this.randomElement(this.allComments))
+
+            pictures.push({
+                src: `../static/img/photos/${i}.jpg`,
+                likes: Math.floor(Math.random() * 100),
                 effect: 'none',
                 description: this.randomElement(this.allDescriptions),
                 comments: comments,
@@ -70,24 +73,26 @@ class PhotoGallery{
     }
     showPictures(){
         const pictureTemplate = document.getElementById('templatePictureExample')
+
         const pictureExample = pictureTemplate.content.querySelector('.pictureExample')
-        this.pictureContainer.innerHtml = '';
+        this.pictureContainer.innerText = ''
 
         this.picturesDB.forEach( (photo) => {
             const photoBlock = pictureExample.cloneNode(true)
-            photoBlock.querySelector('.pictureImg').src = photo.src
+            photoBlock.querySelector('.pictureImg').src = photo.src;
             photoBlock.querySelector('.pictureImg').style.filter = photo.effect
             photoBlock.querySelector('.pictureComments').innerText = photo.commentsNumber
             photoBlock.querySelector('.pictureStars').innerText = photo.likes
-            
-            this.pictureContainer.append(photoBlock)
+
+            this. pictureContainer.append(photoBlock)
+
         })
-        //console.log(pictureExample)
+        // console.log(pictureExample)
     }
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     new MainMenu('.menuTrigger', '.mainMenuContainer')
-    const gallery = new PhotoGallery();
+    const gallery = new PhotoGallery
     gallery.showPictures();
 })
